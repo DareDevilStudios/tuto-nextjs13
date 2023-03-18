6b540024797461d44b14f735127877ba31d43796
Muhammed rinshad s r <81734540+DareDevilStudios@users.noreply.github.com>
2023-03-18 17:08:34 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex w-100 justify-center items-center flex-col h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-6xl text-black font-bold">Next.js + Tailwind CSS template</h1>
+    </main>
+  )
+}
